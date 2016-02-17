4c8085e24766125dd23943ed934eedc6faa289a8
tausendfreundin <anja.schneck@gmail.com>
2016-02-17 22:09:08 +0100

    rocket started

--- js/rocket.js
+++ js/rocket.js
@@ -1,3 +1,22 @@
+var timer = null;
+var countdownNumber = 10;
+
 var changeState = function (state) {
 	document.body.className = 'body-state'+state;
+
+	clearInterval(timer);
+
+	if (state == 2) {
+		timer = setInterval(function () {
+			document.getElementById('countdown').innerHTML = countdownNumber;
+			countdownNumber = countdownNumber - 1;	
+			if (countdownNumber <= 0) {
+				
+				changeState(3);
+			};
+		},  500);
+	
+		} else if (state == 3) {
+
+		};
 }
