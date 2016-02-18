c2109b7d62dd49b3050d17b289190ed6cce5b975
tausendfreundin <anja.schneck@gmail.com>
2016-02-18 11:40:12 +0100

    made my rocket fly

--- js/rocket.js
+++ js/rocket.js
@@ -5,11 +5,16 @@ var changeState = function (state) {
 	document.body.className = 'body-state'+state;
 
 	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById('countdown').innerHTML = countdownNumber;
 
+	//countdown
 	if (state == 2) {
 		timer = setInterval(function () {
-			document.getElementById('countdown').innerHTML = countdownNumber;
 			countdownNumber = countdownNumber - 1;	
+			
+			document.getElementById('countdown').innerHTML = countdownNumber;
+			
 			if (countdownNumber <= 0) {
 				
 				changeState(3);
@@ -17,6 +22,22 @@ var changeState = function (state) {
 		},  500);
 	
 		} else if (state == 3) {
+			var success = setTimeout (function ()
+			{
+				var randomNumber = Math.round (Math.random ()*10);
+
+				console.log ('randomNumber:', randomNumber)
+
+				//success
+				if (randomNumber > 3) {
+					changeState(4);
+
+				}
+				else {
+					changeState(5); //oh no!
+				}
+
+			}, 2000);
 
 		};
 }
